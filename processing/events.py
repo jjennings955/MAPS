26f07867d5d47af6c9ef5cc6627ac0c2027e0eb0
jason <jjennings955@gmail.com>
2015-07-21 08:16:07 -0500

    Got basic ROS node (proof of concept, at least) working.

---
+++ processing/events.py
@@ -0,0 +1,25 @@
+import rospy
+from std_msgs.msg import String
+from calibration import set_threshholds, get_threshhold
+import random
+
+class EventManager(object):
+    def __init__(self):
+        self.publisher = rospy.Publisher('pressure_events', String, queue_size=100)
+        rospy.init_node('MAPS_talker', anonymous=True)
+    def publish_packet(self, sensor_id, sensor_value):
+        threshhold = get_threshhold(sensor_id)
+        if sensor_value > threshhold:
+            self.publisher.publish("Sensor: %d, Pressure: %d" % (sensor_id, sensor_value))
+    def sleep(self):
+        rospy.Rate(10).sleep()
+
+if __name__ == '__main__':
+    try:
+        set_threshholds(dict(zip([str(i) for i in range(10)], [26 for i in range(10)])))
+        eventManager = EventManager()
+        while True:
+            eventManager.publish_packet(random.randint(0,9), random.randint(0,36))
+            eventManager.sleep()
+    except rospy.ROSInterruptException:
+        pass
