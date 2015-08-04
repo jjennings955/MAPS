94b4fe3d068f668852ec3c3ed38a0f4e2528b414
jason <jjennings955@gmail.com>
2015-08-04 07:32:44 -0500

    Implemented data structure for aggregation subsystem.

--- processing/aggregation.py
+++ processing/aggregation.py
@@ -1,4 +1,5 @@
 import rospy
+import numpy as np
 
 def set_windowsize(n=10):
     """Maintain an internal history of n sensor readings"""
@@ -11,4 +12,70 @@ def initialize(n):
 
 def update(which, val):
     rospy.set_param('current_value/%d' % which, val)
-#history = rospy.get_param('/sensor_history', [])
+
+class AggregationBuffer(object):
+    def __init__(self, window_size=1):
+        self.history = np.zeros(dtype=np.float32, shape=(window_size,1))
+        self.window_size = window_size
+        self.head_ptr = 0
+        self.tail_ptr = 0
+        self.full = False
+        self.empty = True
+    def add(self, item):
+        self.empty = False
+        if not self.full:
+            self.history[self.tail_ptr] = item
+            self.tail_ptr += 1
+            if self.tail_ptr == self.window_size:
+                self.full = True
+        else:
+            self.history = np.roll(self.history, -1)
+            self.history[self.window_size - 1] = item
+
+    def window(self):
+        if self.empty:
+            return None
+        if self.full:
+            return self.history
+        else:
+            return self.history[0:self.tail_ptr]
+
+    def mean(self):
+        window = self.window()
+        if window is not None:
+            return np.mean(window)
+
+
+    def median(self):
+        window = self.window()
+        if window is not None:
+            return np.median(window)
+
+    def min(self):
+        window = self.window()
+        if window is not None:
+            return np.min(window)
+
+    def max(self):
+        window = self.window()
+        if window is not None:
+            return np.max(window)
+
+
+
+if __name__ == "__main__":
+    foo = AggregationBuffer(10)
+    print(foo.mean())
+    print(foo.median())
+    print(foo.min())
+    print(foo.max())
+
+    for i in range(10):
+        foo.add(i)
+        print(foo.history)
+    for i in range(5):
+        foo.add(i)
+    print foo.mean()
+    print foo.median()
+    print foo.min()
+    print foo.max()
