7b4a03abddb29d3069c604a45099e3a5ed3b7527
jjennings955 <jjennings955@gmail.com>
2015-08-06 10:34:16 -0500

    Changed file io stuff.

--- presentation/fileinput.py
+++ presentation/fileinput.py
@@ -1,11 +1,11 @@
 __author__ = 'jason'
 class Sensor(object):
-    def __init__(self, sensor, x, y):
+    def __init__(self, sensor, row, col):
         self.sensor_id = sensor
-        self.x = x
-        self.y = y
+        self.row = row
+        self.col = col
     def __repr__(self):
-        return "Sensor(id=%d, x=%d, y=%d)" % (self.sensor_id, self.x, self.y)
+        return "Sensor(id=%d, row=%d, col=%d)" % (self.sensor_id, self.row, self.col)
     def __str__(self):
         return self.__repr__()
     def draw(self):
@@ -15,9 +15,12 @@ def read_layout_file(fname):
     sensors = []
     try:
         with open(fname, 'r') as fd:
-            for line in fd:
-                sensor_id, x_position, y_position = line.split(',')
-                sensors.append(Sensor(int(sensor_id), int(x_position), int(y_position)))
+            for row, line in enumerate(fd):
+                entries = line.split(',')
+                for col, entry in enumerate(entries):
+                    entry = entry.strip()
+                    if entry:
+                        sensors.append(Sensor(int(entry), int(row), int(col)))
     except (ValueError, IOError) as e:
         print "Ignoring bad line in file %s" % (fname)
     return sensors
