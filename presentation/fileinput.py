2e01b575be1417a822a847a484c4c08a0d03d457
MarcusMirzaie <marcus.mirzaie@mavs.uta.edu>
2015-08-06 10:56:40 -0500

    Added max_width and max_height for drawing canvas as well as stub for drawing canvas.

--- presentation/fileinput.py
+++ presentation/fileinput.py
@@ -1,14 +1,25 @@
 __author__ = 'jason'
+
 class Sensor(object):
+    cell_height = 0
+    cell_width = 0
+    max_width = 0
+    max_height = 0
     def __init__(self, sensor, row, col):
         self.sensor_id = sensor
         self.row = row
         self.col = col
+        if self.col > Sensor.max_width:
+            Sensor.max_width = self.col
+        if self.row > Sensor.max_height:
+            Sensor.max_height = self.row
     def __repr__(self):
         return "Sensor(id=%d, row=%d, col=%d)" % (self.sensor_id, self.row, self.col)
     def __str__(self):
         return self.__repr__()
     def draw(self):
+        pass
+    def draw_canvas(self):
         pass
 
 def read_layout_file(fname):
@@ -28,3 +39,4 @@ def read_layout_file(fname):
 if __name__ == "__main__":
     sensor = read_layout_file("whatever.csv")
     print(sensor)
+    print(Sensor.max_height + 1, " ", Sensor.max_width + 1)
